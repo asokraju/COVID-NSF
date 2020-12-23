1118ea12c7c45bda4ebb9dccb7dbbd3c3ebcf253
Krishna Chaitanya Kosaraju <kkrishnachaitanya89@gmail.com>
2020-12-23 14:03:19 -0600

    new

--- eval.py
+++ eval.py
@@ -179,11 +179,20 @@ def plot_act(path, savefig_filename=None):
         percent_a.append(temp.values)
     Act_pd = pd.DataFrame(np.array(percent_a).T, columns = exp_w)
     
-    print(Act_pd)
-    sns.barplot(
-        data= Act_pd,
-        hue = '0.3'
-    )
+    # print(Act_pd)
+    # sns.barplot(
+    #     data= Act_pd,
+    #     hue = '0.3'
+    # )
+    # plt.show()
+
+    data = Act_pd.values
+    X = np.arange(5)
+    fig = plt.figure()
+    ax = fig.add_axes([0,0,1,1])
+    ax.bar(X + 0.00, data[0], color = 'b', width = 0.25)
+    ax.bar(X + 0.25, data[1], color = 'g', width = 0.25)
+    ax.bar(X + 0.50, data[2], color = 'r', width = 0.25)
     plt.show()
     return Act_pd
 
