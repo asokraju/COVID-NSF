BugFix: added 1e-10 to std when normalizing rew

RL_algo/PPO.py
@@ -210,7 +210,7 @@ class PPOAgent:
         discounted_rewards.reverse()
         discounted_rewards = np.array(discounted_rewards)
         discounted_rewards -= np.mean(discounted_rewards) # normalizing the result
-        discounted_rewards /= np.std(discounted_rewards) # divide by standard deviation
+        discounted_rewards /= (np.std(discounted_rewards) + 1e-10) # divide by standard deviation
         return discounted_rewards
 
     def replay(self, states, actions, rewards, predictions):